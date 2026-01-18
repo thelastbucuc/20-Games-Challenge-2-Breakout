507ef6c5b88fbd04a5d7ccd0ce62755e2d5ef6d9
thelastbucuc <yusufdurak939@gmail.com>
2026-01-18 17:19:33 +0300

    changed ball physics

--- Scenes/Ball/Ball.gd
+++ Scenes/Ball/Ball.gd
@@ -6,13 +6,13 @@ const SPEED_MULT: float = 1.03
 
 var _speed : float = 400.0
 var _can_launch: bool = true
-var _rand_x_dir: float = randf_range(-1, 1)
-var _launch_velocity: Vector2 = Vector2(_rand_x_dir, -1).normalized() * _speed
+
 
 func _unhandled_input(event: InputEvent) -> void:
 	if Input.is_action_just_pressed("launch") and _can_launch == true:
 		_can_launch = false
-		velocity = _launch_velocity
+		var _rand_x_dir: float = randf_range(-1, 1)
+		velocity = Vector2(_rand_x_dir, -1).normalized() * _speed
 
 
 # Called when the node enters the scene tree for the first time.
@@ -22,26 +22,19 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
-	update_movement(delta)
-
-
-func update_movement(delta: float) -> void:
 	var collision = move_and_collide(velocity * delta)
 	if collision:
 		var collider: Object = collision.get_collider()
 		if collider.is_in_group(Brick.GROUP_NAME):
 			collider.queue_free()
 		if collider.is_in_group(Paddle.GROUP_NAME):
-			var collision_diff = global_position.x - collider.global_position.x
 			var paddle_width = collider.get_node("CollisionShape2D").shape.size.x
-			velocity.x = (collision_diff / (paddle_width / 2))
-			#clampf(velocity.x, -0.7, 0.7)
-			velocity.y = -abs(velocity.y) 
-			velocity = velocity.normalized() * _speed
-			#bounce_angle = clamp(bounce_angle, -0.7, 0.7)
-			#var new_direction = Vector2(-velocity.normalized().y, bounce_angle).normalized()
-			#velocity = new_direction * _speed
+			var relative_collision_x = (global_position.x - collider.global_position.x) / (paddle_width / 2)
+			if abs(relative_collision_x) < 0.2:
+				relative_collision_x = 0.2 if relative_collision_x >= 0 else -0.2
+			relative_collision_x = clampf(relative_collision_x, -0.8, 0.8)
+			var new_direction = Vector2(relative_collision_x, -1).normalized()
+			velocity = new_direction * _speed
 		else:
 			velocity = velocity.bounce(collision.get_normal())
-		#_speed *= SPEED_MULT
-		print(velocity.x)
+	
